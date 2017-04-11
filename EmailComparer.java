6dcaa0ab16730c64a116e4225a3429d799b30685
Christopher Szatmary <christopherszatmary@Christophers-MacBook-Pro.local>
2017-04-11 11:17:13 -0400

    Removed Comparer classes

--- EmailComparer.java
+++
@@ -1,5 +0,0 @@
-public abstract class EmailComparer {
-  public static int compare(Person first, Person second) {
-    return first.getEmail().compareTo(second.getEmail());
-  }
-}
--- PhoneNumberComparer.java
+++
@@ -1,5 +0,0 @@
-public abstract class PhoneNumberComparer {
-  public static int compare(Person first, Person second) {
-    return first.getTelephoneNumber().compareTo(second.getTelephoneNumber());
-  }
-}
